78b0c93841333ee2a04452e6ce22b687a6a608ba
Ruman 03 <rumanasif3@gmail.com>
2024-09-26 12:24:27 +0500

    udpate!

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -187,7 +187,7 @@ const Resume = () => {
             {/* experience */}
             <TabsContent value='experience' className='w-full '>
               <div className='flex flex-col gap-[30px] text-center xl:text-left'>
-                <h3 className='text-4xl font-bold'>{experience.title}</h3>
+                <h2 className='text-4xl font-bold'>{experience.title}</h2>
                 <p className='max-w-[600px] text-white/60 mx-auto xl:mx-0'>{experience.description}</p>
                 <ScrollArea className='h-[400px]'>
                   <ul className='grid grid-cols-1 lg:grid-cols-2 gap-[30px]'>

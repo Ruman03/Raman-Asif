2c5b8b5edf3e608233d86953ce29e989b965204a
Ruman 03 <rumanasif3@gmail.com>
2024-09-29 19:22:44 +0500

    Update

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -38,7 +38,7 @@ const Contact = () => {
             {/* form */}
             <div className="xl:w-[54%] order-2 xl:order-none">
               <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl ">
-                <h2 className="text-4xl text-accent">Let's Work Together</h2>
+                <h1 className="text-4xl text-accent">Let's Work Together</h1>
                 <p className="text-white/60 ">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est, temporibus. Accusantium dolorem nisi sapiente.</p>
                 {/* input */}
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">

b26c266ba0e959339a8e92730da26a683dbb8de5
Ruman 03 <rumanasif3@gmail.com>
2024-09-29 19:19:13 +0500

    Ruman Site

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -174,7 +174,7 @@ const Resume = () => {
     <motion.div  initial={{opacity:0}} animate={{opacity: 1 , transition: {delay:0.3, duration: 0.4, ease: 'easeIn'} }} 
     className='min-h-[80vh] flex items-center justify-center py-12 xl:py-0' >
       <div className="container mx-auto">
-        <Tabs defaultValue='experience' className='flex flex-col xl:flex-row gap-[60px]'>
+        <Tabs defaultValue='skills' className='flex flex-col xl:flex-row gap-[60px]'>
           <TabsList className='flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6'>
             <TabsTrigger value='skills'>Skills</TabsTrigger>
             <TabsTrigger value='education'>Education</TabsTrigger>
